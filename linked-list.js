3a17bec4def14a534076d164490d4cd3722d73ca
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2025-01-14 21:47:46 +0700

    create main and linked-ilist js and insert first node done

---
+++ linked-list.js
@@ -0,0 +1,21 @@
+class Node {
+    constructor(data, next = null) {
+        this.data = data
+        this.next = next
+    }
+}
+
+const n1 = new Node(100)
+console.log(n1)
+
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.size = 0
+    }
+
+    //insert first node
+    insertFirst(data) {
+        this.head = new Node(data, this.head)
+    }
+}
